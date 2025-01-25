1d3ff81ae4098cafafcf66ad9ee19e5c7723f3ed
Divyansh Singh <115529968+divyanshsingh07@users.noreply.github.com>
2025-01-26 00:28:52 +0530

    Time: 0 ms (100.00%) | Memory: 40.8 MB (82.33%) - LeetSync

--- 69-sqrtx/sqrtx.java
+++ 69-sqrtx/sqrtx.java
@@ -1,25 +1,6 @@
 class Solution {
     public int mySqrt(int x) {
-        if (x == 0) {
-            return 0;
-        } else if (x == 1) {
-            return 1;
-        }
-
-        double num = x / 2;
-        double prevNum;
-
-        while (true) {
-
-            prevNum = num;
-            num = (num + (x / num)) / 2;
-
-            if (Math.floor(prevNum) % Math.floor(num) == 0.0d) {
-                break;
-            }
-        }
-
-        int ans = (int) Math.floor(num);
+        int ans=(int)Math.sqrt(x);
         return ans;
     }
 }
