2685d1d17a545b53155aac94a5888bec3e6d435a
Divyansh Singh <115529968+divyanshsingh07@users.noreply.github.com>
2025-02-08 00:26:39 +0530

    Time: 41 ms (60.70%) | Memory: 81.7 MB (84.72%) - LeetSync

--- 3434-find-the-number-of-distinct-colors-among-the-balls/find-the-number-of-distinct-colors-among-the-balls.java
+++ 3434-find-the-number-of-distinct-colors-among-the-balls/find-the-number-of-distinct-colors-among-the-balls.java
@@ -1,30 +1,26 @@
 class Solution {
     public int[] queryResults(int limit, int[][] queries) {
-        int n = queries.length;
-        int[] result = new int[n];
-        
-        Map<Integer, Integer> colormp = new HashMap<>(); // color -> count
-        Map<Integer, Integer> ballmp = new HashMap<>(); // ball -> color
-        
-        for (int i = 0; i < n; i++) {
-            int ball = queries[i][0];
-            int color = queries[i][1];
-            
-            if (ballmp.containsKey(ball)) { // already colored ball
-                int prevColor = ballmp.get(ball);
-                colormp.put(prevColor, colormp.get(prevColor) - 1);
-                
-                if (colormp.get(prevColor) == 0) {
-                    colormp.remove(prevColor);
+        Map<Integer,Integer> node = new HashMap<>();
+        Map<Integer,Integer> color = new HashMap<>();
+        int ans[]=new int[queries.length];
+        for(int i=0;i<queries.length;i++){
+            int it[]=queries[i];
+            if(node.containsKey(it[0])){
+                if(node.get(it[0])==it[1]) {
+                    ans[i]=color.size();continue;
                 }
+                else if (color.get(node.get(it[0]))<=1) color.remove(node.get(it[0]));
+                else
+                color.put(node.get(it[0]),color.get(node.get(it[0]))-1);
+                node.put(it[0],it[1]);
+                color.put(it[1],color.getOrDefault(it[1],0)+1);
             }
-            
-            ballmp.put(ball, color);
-            colormp.put(color, colormp.getOrDefault(color, 0) + 1);
-            
-            result[i] = colormp.size();
+            else{
+                node.put(it[0],it[1]);
+                color.put(it[1],color.getOrDefault(it[1],0)+1);
+            }
+            ans[i]=color.size();
         }
-        
-        return result;
+        return ans;
     }
 }
