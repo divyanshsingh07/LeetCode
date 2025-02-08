2ed8b1433078c9e2dd1f4d983e67907f3113f854
Divyansh Singh <115529968+divyanshsingh07@users.noreply.github.com>
2025-02-08 16:56:47 +0530

    Time: 2 ms (88.22%) | Memory: 55.3 MB (74.74%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -1,20 +1,21 @@
 class Solution {
-    static {
-        for (int i = 0; i < 500; i++) {
-            productExceptSelf(new int[]{0, 0});
+    public int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int[] result = new int[n];
+        
+        // Step 1: Calculate left products and store them in the result array
+        result[0] = 1;
+        for (int i = 1; i < n; i++) {
+            result[i] = result[i - 1] * nums[i - 1];
         }
-    }
-    public static int[] productExceptSelf(int[] nums) {
-        int answer[] = new int[nums.length];
-        answer[0] = 1;
-        for(int i=1; i<nums.length; i++){
-            answer[i] = answer[i-1] * nums[i-1];
+
+        // Step 2: Calculate right products and update the result array in place
+        int rightProduct = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            result[i] = result[i] * rightProduct;
+            rightProduct *= nums[i];
         }
-        int suffixProduct = 1;
-        for(int i=nums.length-1; i>=0; i--){
-            answer[i] *= suffixProduct;
-            suffixProduct *= nums[i];
-        }
-        return answer;
+
+        return result;
     }
 }
