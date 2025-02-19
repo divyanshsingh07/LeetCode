061f72e2f28c4db776e17a5e9f03952c022ea7a7
Divyansh Singh <115529968+divyanshsingh07@users.noreply.github.com>
2025-02-20 00:55:27 +0530

    Time: 31 ms (58.61%) | Memory: 51.4 MB (83.66%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,19 +1,32 @@
+import java.util.*;
+
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        int n=nums.length;
+        int n = nums.length;
+        List<List<Integer>> rs = new ArrayList<>();
+
+        // Sort the array to easily handle duplicates and use two-pointer approach
         Arrays.sort(nums);
-        List<List<Integer> >rs=new ArrayList<>();
-        for(int i=0;i<n-2;i++){
-            if(i>0&&nums[i]==nums[i-1]) continue;
-            int l=i+1;
-            int r=n-1;
-            while(l<r){
-                int s=nums[i]+nums[l]+nums[r];
-                if(s==0){
+
+        for (int i = 0; i < n - 2; i++) {
+            // Skip duplicate elements for the first number
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+
+            int l = i + 1;
+            int r = n - 1;
+
+            while (l < r) {
+                int s = nums[i] + nums[l] + nums[r];
+
+                if (s == 0) {
+                    // Correctly add the triplet
                     rs.add(Arrays.asList(nums[i], nums[l], nums[r]));
 
-                   while (l < r && nums[l] == nums[l + 1]) l++;
+                    // Skip duplicate elements for the second and third numbers
+                    while (l < r && nums[l] == nums[l + 1]) l++;
                     while (l < r && nums[r] == nums[r - 1]) r--;
+
+                    // Move pointers after finding a valid triplet
                     l++;
                     r--;
                 } else if (s < 0) {
@@ -22,9 +35,8 @@ class Solution {
                     r--;
                 }
             }
-        
         }
+
         return rs;
     }
-
 }
