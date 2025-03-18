6bf53f2d7fa2a75f8fb8a67d502e7a3d38b6c417
Divyansh Singh <115529968+divyanshsingh07@users.noreply.github.com>
2025-03-19 01:10:08 +0530

    Time: 3 ms (98.67%) | Memory: 56.1 MB (23.87%) - LeetSync

--- 2478-longest-nice-subarray/longest-nice-subarray.java
+++ 2478-longest-nice-subarray/longest-nice-subarray.java
@@ -4,12 +4,12 @@ class Solution {
         int n = nums.length;
 
         for (int i = 0; i < n; i++) {
-            int bitMask = 0;  
+            int r = 0;  
             for (int j = i; j < n; j++) {
-                if ((bitMask & nums[j]) != 0) { 
+                if ((r & nums[j]) != 0) { 
                     break;
                 }
-                bitMask |= nums[j]; 
+                r |= nums[j]; 
                 maxLen = Math.max(maxLen, j - i + 1);
             }
         }
