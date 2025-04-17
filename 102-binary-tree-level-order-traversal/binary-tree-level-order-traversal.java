09b6ec5a13365970350362e10dd024818237d991
Divyansh Singh <115529968+divyanshsingh07@users.noreply.github.com>
2025-04-17 19:46:18 +0530

    Time: 1 ms (89.50%) | Memory: 45.2 MB (36.42%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
@@ -29,8 +29,8 @@ class Solution {
                 TreeNode node = q.poll();
                 level.add(node.val);
 
-                if (node.left != null) q.offer(node.left);
-                if (node.right != null) q.offer(node.right);
+                if (node.left != null) q.add(node.left)  ; 
+                if (node.right != null) q.add(node.right);
             }
 
             result.add(level);
