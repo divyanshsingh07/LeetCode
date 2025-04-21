b6a1d9fe542eb92d4a1838ca328ca2c399ec7444
Divyansh Singh <115529968+divyanshsingh07@users.noreply.github.com>
2025-04-21 15:36:59 +0530

    Time: 16 ms (32.04%) | Memory: 45.2 MB (92.84%) - LeetSync

--- 1508-longest-happy-prefix/longest-happy-prefix.java
+++ 1508-longest-happy-prefix/longest-happy-prefix.java
@@ -1,20 +1,24 @@
-public class Solution {
+class Solution {
     public String longestPrefix(String s) {
+        long prefixHash = 0, suffixHash = 0;
+        int best = 0;
+        long base = 31, mod = 1_000_000_007, power = 1;
         int n = s.length();
-        int[] lps = new int[n];  // KMP prefix table
-        int len = 0; // length of previous longest prefix suffix
 
-        for (int i = 1; i < n; i++) {
-            while (len > 0 && s.charAt(i) != s.charAt(len)) {
-                len = lps[len - 1]; // fallback
+        for (int i = 0; i < n - 1; i++) {
+            int leftChar = s.charAt(i) - 'a' + 1;
+            int rightChar = s.charAt(n - 1 - i) - 'a' + 1;
+
+            prefixHash = (prefixHash * base + leftChar) % mod;
+            suffixHash = (suffixHash + power * rightChar) % mod;
+
+            if (prefixHash == suffixHash) {
+                best = i + 1;
             }
 
-            if (s.charAt(i) == s.charAt(len)) {
-                len++;
-                lps[i] = len;
-            }
+            power = (power * base) % mod;
         }
 
-        return s.substring(0, lps[n - 1]);
+        return s.substring(0, best);
     }
 }
